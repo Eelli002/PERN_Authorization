b2f4aa1962c2b5992b9f5d38a2266831b37a75dc
eelli002 <elijah_elliott@hotmail.com>
2023-01-03 19:47:32 -0800

    Register User

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -1,5 +1,40 @@
 const router = require('express').Router();
+const pool = require('./../db');
+const bcrypt = require('bcrypt');
 
-// Registering
+// Register New User
+router.post('/register', async (req, res) => {
+    try 
+    {
+        // Destructure our req object for user name, email, & password.
+        const { name, email, password } = req.body;
+
+        // Check if the user exists, else throw error.
+        const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [email]);
+        if (user.rows.length !== 0) {
+            return res.status(401).send("User already exists");
+        }
+
+        // Hash the users password before saving in postgres using Bcrypt
+        const saltRound = 10;
+        const salt = await bcrypt.genSalt(saltRound); // Create a salt(string appended to password before hashing)
+        const hashedPassword = await bcrypt.hash(password, salt); // Append salt to user password and Hash together
+
+        // Insert user info into postgres.
+        const newUser = 
+            await pool.query(
+                "INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *",
+                [name, email, hashedPassword]
+            );
+        res.json(newUser.rows[0]);
+
+        // Generate our JWT
+    } 
+    catch (error) 
+    {
+        console.error(error.message);
+        res.status(500).send("Server Error");
+    }
+})
 
 module.exports = router;
