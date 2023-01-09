180089ba9ca1b359f6a029dbe1b19e36f37adaa6
eelli002 <elijah_elliott@hotmail.com>
2023-01-09 10:18:15 -0800

    Register Component Working

--- client/src/components/register.js
+++ client/src/components/register.js
@@ -1,10 +1,63 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Register = () => {
+const Register = ({Set_Auth}) => {
+
+    const [inputs, set_inputs] = useState({ name:'', email:'', password:'' });
+
+    const { name, email, password } = inputs;
+
+    const onChange = e => set_inputs({ ...inputs, [e.target.name] : e.target.value });
+    // Take inputs and change state of name to value.     ie: name:"Elijah"(value)
+
+    const On_Submit_Form = async e => {
+        e.preventDefault();
+        try 
+        {
+            const body = { name, email, password }
+            const response = 
+                await fetch(
+                    'http://localhost:3002/auth/register', 
+                    { method:'POST', headers:{'Content-Type':'application/json'}, body: JSON.stringify(body)} 
+                )
+            const parsed_response = await response.json();
+            localStorage.setItem('token', parsed_response['token']);
+            Set_Auth(true);
+        } 
+        catch (error) {
+            console.error(error.message)
+        }
+    }
+
     return (
         <>
-            <div></div>
-            <h1>Register</h1>
+            <h1 className='text-center my-5'>Register</h1>
+            <form onSubmit={On_Submit_Form}>
+                <input 
+                    type='text' 
+                    name='name' 
+                    placeholder='name' 
+                    className='form-control my-3' 
+                    value={name}
+                    onChange={ e => onChange(e) }
+                />
+                <input 
+                    type='email' 
+                    name='email' 
+                    placeholder='email' 
+                    className='form-control my-3'
+                    value={email}
+                    onChange={ e => onChange(e) }
+                />
+                <input 
+                    type='password' 
+                    name='password' 
+                    placeholder='password' 
+                    className='form-control my-3'
+                    value={password}
+                    onChange={ e => onChange(e) }
+                />
+                <button className='btn-success btn-block'>Submit</button>
+            </form>
         </>
     );
 }
