957c6952d128f2c98c59cab028d53ee198fd77b5
eelli002 <elijah_elliott@hotmail.com>
2023-01-11 13:29:38 -0800

    Dashboard Component Working

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Dashboard = ({ Set_Auth }) => {
+
+    const [name, setName] = useState('');
+
+    const Get_Name = async () => {
+        try 
+        {
+            const response = 
+                await fetch(
+                    'http://localhost:3002/dashboard/',
+                    { 'method':'GET', 'headers': { 'token':localStorage.token } }
+                );
+            const parsed_response = await response.json();
+            setName(parsed_response.user_name);
+            
+        } 
+        catch (error) {
+            console.error(error.message);
+        }
+    }
+
+    useEffect( () => {
+        Get_Name();
+    });
+
+    const Logout = (e) => {
+        e.preventDefault();
+        localStorage.removeItem('token');
+        Set_Auth(false);
+    }
+
     return (
         <>
-            <h1>Dashboard</h1>
-            <button onClick={() => Set_Auth(false)}>Log Out</button>
+            <h1>{name}'s Dashboard </h1>
+            <button className='btn btn-primary' onClick={ e => Logout(e) }>Log out</button>
         </>
     )
 }
